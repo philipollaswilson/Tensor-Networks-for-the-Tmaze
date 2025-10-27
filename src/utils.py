04a30730aab4371baed78d027a3752495c4477a8
Benjamin Weinmann <3141514142@protonmail.com>
2025-10-27 16:07:48 +0100

    adapted utils.py file for this repo (exluded left cano stuff and the functions now expect list of nodes instead of MPS_c objects)

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 import tensornetwork as tn
 import numpy as np
-from ../lib/UnsupGenModbyMPS.MPScumulant import MPS_c
-
+from lib.UnsupGenModbyMPS.MPScumulant import MPS_c
+from typing import Optional
 
 def init_mps(dataset, config):
     mps = MPS_c(space_size=config["n_features"]+1)
@@ -17,96 +17,14 @@ def init_mps(dataset, config):
     return mps
 
 
-def check_mps(mps):
-    mps.left_cano()
-    tensors = mps.matrices
-    # Check orthogonality for left-canonicalized MPS
-    for i, tensor in enumerate(tensors):
-        tensor_shape = tensor.shape
-        tensor = tensor.reshape((tensor_shape[0] * tensor_shape[1], tensor_shape[2]))
-        tensor_conj = np.conj(tensor.T)
-        identity = np.dot(tensor_conj, tensor)
-        assert np.allclose(identity, np.eye(identity.shape[0])), f"Tensor {i} is not orthogonal"
-    
-    # Check norm of the MPS
-    nodes = [tn.Node(tensor) for tensor in tensors]
-    conj_nodes = [tn.Node(np.conj(tensor)) for tensor in tensors]
-    
-    nodes[0][0] ^ conj_nodes[0][0]
-    nodes[-1][2] ^ conj_nodes[-1][2]
-    
-    # Connect the nodes and their conjugates
-    for j in range(len(nodes) - 1):
-        nodes[j][2] ^ nodes[j + 1][0]
-        conj_nodes[j][2] ^ conj_nodes[j + 1][0]
-    
-    # Connect the physical indices
-    for j in range(len(nodes)):
-        nodes[j][1] ^ conj_nodes[j][1]
-    
-    # Contract the network
-    mps_norm = tn.contractors.greedy(nodes + conj_nodes).tensor
-    norm = np.linalg.norm(mps_norm)
-    assert np.isclose(norm, 1.0), f"MPS norm is not 1: {norm}"
-    
-    print("MPS tensors are correct")
-    return True
-
-
 def shannon_entropy(probabilities, base=2):
     """Compute Shannon entropy of a probability distribution"""
     return -np.sum(probabilities * np.log(probabilities + 1e-12) / np.log(base))
 
 
-def compute_entanglement_given_fixed_label(
-        tensors,
-        label_site,
-        feature_site,
-        fixing_tensor,
-):
-    
-    # Contract the MPS tensors to compute the RDM
-    nodes = [tn.Node(tensor) for tensor in tensors]
-    conj_nodes = [tn.Node(np.conj(tensor)) for tensor in tensors]
-    
-    # Connect the extremes of the MPS
-    nodes[feature_site][0] ^ conj_nodes[feature_site][0]
-    nodes[label_site][2] ^ conj_nodes[label_site][2]
-    
-    # Connect the edges of the MPS
-    for i in range(feature_site + 1, label_site):
-        # Connect the physical indices
-        nodes[i][1] ^ conj_nodes[i][1]
-        
-        # Connect the bond indices
-        nodes[i - 1][2] ^ nodes[i][0]
-        conj_nodes[i - 1][2] ^ conj_nodes[i][0]
-        
-    # Connect the bond indices of label site
-    nodes[label_site - 1][2] ^ nodes[label_site][0]
-    conj_nodes[label_site - 1][2] ^ conj_nodes[label_site][0]
-
-    # Connect the fixing tensor
-    fixing_node = tn.Node(fixing_tensor)
-    nodes[label_site][1] ^ fixing_node[0]
-    conj_nodes[label_site][1] ^ fixing_node[1]
-
-    # Contraction gives the RDM after normalization
-    rhoNode = tn.contractors.auto(
-        nodes[feature_site:] + [fixing_node] + conj_nodes[feature_site:],
-        output_edge_order=[nodes[feature_site][1],   
-        conj_nodes[feature_site][1],
-    ])
-    rho = rhoNode.tensor
-    rho_normalized = rho/np.trace(rho)
-    eigenvalues = np.linalg.eigvalsh(rho_normalized)
-    entanglement = shannon_entropy(eigenvalues)
-    
-    return entanglement
-
-
 def compute_RDM(
-        mps, 
+        nodes: list[tn.Node],
+        conj_nodes: list[tn.Node],
         open_edge_idxs: list[int],
         fixing_nodes: list[tn.Node] = [],
         fixing_sites: list[int] = [],
@@ -116,11 +34,12 @@ def compute_RDM(
     assert set(fixing_sites).issubset(set(open_edge_idxs)), "fixing_sites must be a subset of open_edge_idxs"
     fixing_flag = False
     if len(fixing_nodes) > 0: fixing_flag = True
-    
-    mps.left_cano()
-    tensors = mps.matrices
-    nodes = [tn.Node(tensor) for tensor in tensors]
-    conj_nodes = [tn.Node(np.conj(tensor)) for tensor in tensors]
+
+    # Due to canonicalization, we can truncate the MPS to the maximum open edge index
+
+    max_site = max(open_edge_idxs)
+    nodes = nodes[:max_site + 1]
+    conj_nodes = conj_nodes[:max_site + 1]
 
     # Connect the extremes of the MPS
     nodes[0][0] ^ conj_nodes[0][0]
@@ -170,34 +89,40 @@ def compute_RDM(
     # Reshape to a rank-2 tensor where the dimensions are products of the respective open edges
     RDM = contracted_mps_tensor.reshape((first_dim_product, second_dim_product))
     
-    # Normalize the tensor if fixing_flag is set
-    if fixing_flag: RDM /= np.trace(RDM)
+    RDM /= np.trace(RDM)
     
     # contracted mps tensor not normalized!
     return RDM, contracted_mps_tensor
 
 
 def compute_mutual_information(
-        mps,
+        nodes: list[tn.Node],
+        conj_nodes: list[tn.Node],
         edge_idx_i: int,
         edge_idx_j: int,
-        S_j: float = -1.0,
+        S_j: Optional[float] = None
     ):
+    """
+        Compute the mutual information between two edges in the MPS.
+    """
     RDM_ij, _ = compute_RDM(
-        mps,
+        nodes,
+        conj_nodes,
         open_edge_idxs=[edge_idx_i, edge_idx_j],
     )
     S_ij = shannon_entropy(np.linalg.eigvalsh(RDM_ij))
     RDM_i, _ = compute_RDM(
-        mps,
+        nodes,
+        conj_nodes,
         open_edge_idxs=[edge_idx_i],
     )
     S_i = shannon_entropy(np.linalg.eigvalsh(RDM_i))
     RDM_j, _ = compute_RDM(
-        mps,
+        nodes,
+        conj_nodes,
         open_edge_idxs=[edge_idx_j],
     )
-    if S_j < 0:
+    if S_j is None:
         S_j = shannon_entropy(np.linalg.eigvalsh(RDM_j))
     mutual_information = (S_i + S_j - S_ij)/2
     return mutual_information
